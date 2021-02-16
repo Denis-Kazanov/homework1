5458d2363d7e2dc0816669330c2080407e509ab0
Denis-Kazanov <kaz-dan@mail.ru>
2021-02-16 13:42:11 +0300

    Выполненое дз

---
+++ src/homework11/Main.java
@@ -0,0 +1,9 @@
+package homework11;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculate test = new Calculate(1, 4);
+        System.out.println(test.toString());
+        System.out.println(test.value());
+    }
+}
