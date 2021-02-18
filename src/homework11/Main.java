1e90da8da4fb9721aecd71dcf7b0c09e293b9881
Denis-Kazanov <kaz-dan@mail.ru>
2021-02-18 22:18:29 +0300

    Урок 12 +

--- src/homework11/Main.java
+++ src/homework11/Main.java
@@ -2,7 +2,7 @@ package homework11;
 
 public class Main {
     public static void main(String[] args) {
-        Calculate test = new Calculate(1, 4);
+        Calculate test = new Calculate(3, 7);
         System.out.println(test.toString());
         System.out.println(test.value());
     }
